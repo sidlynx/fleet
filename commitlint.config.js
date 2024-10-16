c4986664f312a34bb2c20378dd39a59307034756
Mohammed Hamdoune <sidlynx@gmail.com>
2024-10-16 22:12:05 +0200

    chore(repo): configure husky with commitlint and commitizen for better commit messages

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "scope-empty": [
+            2,
+            "never",
+        ],
+        "scope-enum": [
+            2,
+            "always",
+            [
+                "repo",
+            ],
+        ],
+    },
+};
