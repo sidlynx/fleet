32216df628a6dab365197decf4329c339fc5e5b7
Mohammed Hamdoune <sidlynx@gmail.com>
2024-10-17 12:08:28 +0200

    chore(api): add trpc support

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
+import { appRouter } from './trpc/router';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    '/rpc',
+    createExpressMiddleware({
+      router: appRouter,
+    }),
+  );
+
   await app.listen(3000);
+
+
 }
 bootstrap();
